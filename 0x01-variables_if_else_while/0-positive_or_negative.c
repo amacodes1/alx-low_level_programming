71ee803773dd7b4a834b47f3b25965cf5a285848
mercyama <mercyezihe2@gmail.com>
2023-03-17 15:36:33 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,18 +11,19 @@
 int main(void)
 {
 int n;
-int lastDigit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+if (n > 0)
 
-lastDigit = n % 10;
-printf("Last digit of %i is %i and is ", n, lastDigit);
-if (lastDigit > 5)
-printf("greater than 5\n");
-else if (lastDigit == 0)
-printf("0\n");
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
 else
-printf("less than 6 and not 0\n");
+printf("%d is negative\n", n);
 return (0);
 }
