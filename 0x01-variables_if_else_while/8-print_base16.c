623e21c4e723063b2f2ca82cea7420a201109d11
mercyama <mercyezihe2@gmail.com>
2023-03-17 17:13:21 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,8 +11,8 @@ int main(void)
 	int i;
 
 	for (i = 48; i < 58; i++)
-	putchat(i);
-	for (i = 'a'; i <= 'f', i++)
+	putchar(i);
+	for (i = 'a'; i <= 'f'; i++)
 	putchar(i);
 	putchar('\n');
 	return (0);
